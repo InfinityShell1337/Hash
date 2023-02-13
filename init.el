f99116fc2cdde60a7fed8a8bbfaefebc399d6100
Infinity <InfinityShell1337@github.com>
2023-02-13 18:25:09 -0500

    Updated

--- init.el
+++ init.el
@@ -516,3 +516,16 @@
 (add-to-list 'default-frame-alist '(alpha . (80 . 90)))
 (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(buffer-flip smartparens minimap beacon emojify undo-tree typescript-mode rust-mode company-box company lsp-ivy lsp-treemacs lsp-ui lsp-mode org-roam org-present visual-fill-column org-bullets forge magit counsel-projectile projectile all-the-icons-dired dired-single hydra evil-nerd-commenter evil-collection evil which-key general helpful doom-modeline rainbow-delimiters counsel ivy-posframe all-the-icons-ivy-rich all-the-icons-ivy ivy-rich ivy eshell-prompt-extras eshell-up eshell-z esh-help eshell-did-you-mean all-the-icons auto-package-update doom-themes no-littering use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
